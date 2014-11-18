c094295731553e427d9f6e49c4770c0ab89080e3
Devin Brown <devin.g.brown@gmail.com>
2014-11-18 11:12:58 -0700

    add basic seed data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+item1 = Item.create({title: "magic po", url: "http://lorempixel.com/640/640/cats/", description: "a lovely day"})
+item2 = Item.create({title: "dangerous po", url: "http://lorempixel.com/640/640/cats/", description: "magic mountain"})
+item3 = Item.create({title: "juicy po", url: "http://lorempixel.com/640/640/cats/", description: "some things"})
+
+item4 = Item.create({title: "savory po", url: "http://lorempixel.com/640/640/cats/", description: "a lovely day"})
+item5 = Item.create({title: "hard po", url: "http://lorempixel.com/640/640/cats/", description: "a lovely day"})
+item6 = Item.create({title: "soft po", url: "http://lorempixel.com/640/640/cats/", description: "a lovely day"})
+
+item7 = Item.create({title: "shalom po", url: "http://lorempixel.com/640/640/cats/", description: "a lovely day"})
+item8 = Item.create({title: "candy po", url: "http://lorempixel.com/640/640/cats/", description: "a lovely day"})
+item9 = Item.create({title: "laughing po", url: "http://lorempixel.com/640/640/cats/", description: "a lovely day"})
+
+item1.tags << Tag.create(name: "bacon")
+item2.tags << Tag.create(name: "cats")
+item3.tags << Tag.create(name: "candy")
+
+item4.tags << Tag.where(name: "bacon")
+item5.tags << Tag.where(name: "cats")
+item6.tags << Tag.where(name: "candy")
+
+item7.tags << Tag.where(name: "bacon")
+item8.tags << Tag.where(name: "cats")
+item9.tags << Tag.where(name: "candy")
