fa356038dcb287f1fdfb93ab68814dcfaff3f6d9
Devin Brown <devin.g.brown@gmail.com>
2014-11-18 10:17:16 -0700

    whitespace

--- db/migrate/20141118164846_create_items.rb
+++ db/migrate/20141118164846_create_items.rb
@@ -1,8 +1,8 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-      t.string :title
-      t.string :description
+      t.string  :title
+      t.string  :description
       t.integer :likes
 
       t.timestamps
